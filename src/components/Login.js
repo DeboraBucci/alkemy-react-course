d5034de5c4ea2fae3a9816f050bca9513ce923ad
Debora Bucci <dbucci@protonmail.com>
2022-04-13 17:55:45 -0300

    add validation to the Login's form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,36 @@
 import React from "react";
 
 const Login = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (email === "" || password === "") {
+      console.log("Los campos no deben estar vacíos.");
+      return;
+    }
+
+    if (email !== "" && !regexEmail.test(email)) {
+      console.log("El formato del correo electrónico es inválido.");
+    }
+
+    if (email !== "challenge@alkemy.org" || password !== "react") {
+      console.log("Credenciales inválidas.");
+      return;
+    }
+
+    console.log("Listos para enviar la información.");
+  };
+
   return (
     <React.Fragment>
       <h2>Formulario de login</h2>
-      <form>
+      <form onSubmit={submitHandler}>
         <label htmlFor="email">Correo electrónico:</label>
         <br />
         <input type="email" name="email"></input>
