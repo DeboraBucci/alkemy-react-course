13177ca772ead7c87be6170c0a0442a85fb8a32a
Debora Bucci <dbucci@protonmail.com>
2022-04-13 19:20:56 -0300

    create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.google.com/" rel="noopener noreferrer">
+              Google Link
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <p>Copyright&copy; Débora Bucci </p>
+    </footer>
+  );
+};
+
+export default Footer;
