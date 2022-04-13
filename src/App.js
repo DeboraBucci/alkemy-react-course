a06b31c94d4350c75c769b3c8608e2702ac797f9
Debora Bucci <dbucci@protonmail.com>
2022-04-13 19:24:38 -0300

    add the Header and Footer components to the App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
 import Listado from "./components/Listado";
 import Login from "./components/Login";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Login />} />
-      <Route path="/listado" element={<Listado />} />
-    </Routes>
+    <React.Fragment>
+      <Header />
+
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/listado" element={<Listado />} />
+      </Routes>
+
+      <Footer />
+    </React.Fragment>
   );
 }
 
