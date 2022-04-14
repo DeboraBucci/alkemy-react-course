0533f13945237c329f4bec8be3457531d9513d53
Debora Bucci <dbucci@protonmail.com>
2022-04-13 22:15:37 -0300

    add structure for the movies' cards

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Listado = () => {
   const navigate = useNavigate();
@@ -11,7 +11,26 @@ const Listado = () => {
     }
   }, [navigate, token]);
 
-  return <h2>Listado</h2>;
+  return (
+    <div className="row">
+      <div className="col-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Movie title</h5>
+            <p className="card-text">
+              Review: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Sequi, suscipit? Veniam dicta, autem id est quod a! Cum, quisquam
+              alias.
+            </p>
+            <Link to="/" className="btn btn-primary">
+              See details
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Listado;
