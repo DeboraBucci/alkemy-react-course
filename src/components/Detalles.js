1673ae8bca91b558ab87b976e12a087d70823c43
Debora Bucci <dbucci@protonmail.com>
2022-04-14 11:36:34 -0300

    get the movie data using an axios call and render it inside the 'Detalles' component

--- src/components/Detalles.js
+++ src/components/Detalles.js
@@ -1,13 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Detalles = () => {
   const token = sessionStorage.getItem("token");
+  const query = new URLSearchParams(window.location.search);
+  const movieID = query.get("movieID");
+  const apiKey = "8dd249658a5c2a41a4340dbc40d4cfd5";
+  const language = "es-ESS";
+  const endPoint = `https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}&language=${language}`;
+
+  const [movie, setMovie] = useState(null);
+  console.log(window.location.search);
+
+  useEffect(() => {
+    axios
+      .get(endPoint)
+      .then((response) => {
+        const movideData = response.data;
+        setMovie(movideData);
+      })
+      .catch((err) => {
+        Swal.fire("Algo salió mal. Por favor, intente más tarde.");
+      });
+  }, [endPoint]);
 
   return (
     <React.Fragment>
       {!token && <Navigate to="/" />}
-      <h2>Detalles de la película</h2>
+      {movie && (
+        <>
+          <h2>Título: {movie.title}</h2>
+          <div className="row">
+            <div className="col-4">
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                className="img-fluid"
+                alt={`${movie.title} poster`}
+              />
+            </div>
+            <div className="col-8">
+              <h5>Fecha de estreno: {movie.release_date}</h5>
+              <h5>Reseña:</h5>
+              <p>{movie.overview}</p>
+              <h5>Puntaje:</h5>
+              <p>{movie.vote_average}</p>
+            </div>
+            <h5>Géneros:</h5>
+            <ul>
+              {movie.genres.map(({ name }) => (
+                <li>{name}</li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </React.Fragment>
   );
 };
