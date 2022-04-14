e1c28c24443e23c9943ac1769c6bfe9e6e696ede
Debora Bucci <dbucci@protonmail.com>
2022-04-13 23:43:06 -0300

    use the movies' data and display it dynamically into each card

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -22,22 +22,26 @@ const Listado = () => {
       {!token && <Navigate to="/" />}
 
       <div className="row">
-        <div className="col-3">
-          <div className="card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Movie title</h5>
-              <p className="card-text">
-                Review: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sequi, suscipit? Veniam dicta, autem id est quod a! Cum,
-                quisquam alias.
-              </p>
-              <Link to="/" className="btn btn-primary">
-                See details
-              </Link>
+        {moviesList.map((movie, i) => {
+          return (
+            <div className="col-3" key={`movie-${i}`}>
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{movie.title}</h5>
+                  <p className="card-text">{movie.overview.slice(0, 100)}...</p>
+                  <Link to="/" className="btn btn-primary">
+                    See details
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </React.Fragment>
   );
